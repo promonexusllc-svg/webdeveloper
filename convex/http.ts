e43f3cf71b5c62b434576badc0f12991bd98cb38
Viktor AI <viktor@getviktor.com>
2026-04-24 07:25:41 +0000

    Remove seed endpoint after admin account created

--- convex/http.ts
+++ convex/http.ts
@@ -1,14 +1,7 @@
 import { httpRouter } from "convex/server";
 import { auth } from "./auth";
-import { seedAdminEndpoint } from "./seedAdminHttp";
 
 const http = httpRouter();
 auth.addHttpRoutes(http);
-
-http.route({
-  path: "/seed-admin",
-  method: "POST",
-  handler: seedAdminEndpoint,
-});
 
 export default http;
